e32d5772f523e2dce7fd9d96b65516f0e6e09985
Tsuyoshi-Toughy <ogutsuyoshi@gmail.com>
2021-01-26 23:13:45 +0900

    edit

--- character.rb
+++ character.rb
@@ -1,3 +1,6 @@
+
+require "./character.rb"
+
 class Character
 
   attr_reader :name , :offense, :defense
@@ -11,19 +14,37 @@ class Character
   end
 end
 
+require "./brave.rb"
 
 class Brave < Character
   def attack(monster)
-    puts "#{monster.name}があらわれた!"
     puts "#{brave.name}の攻撃!"
     damage = (offense - monster.defense) / 2
+    monster.hp = monster.hp - damage
 end
+
+require "./brave.rb"
 
 class Monster < Character
   def attack(brave)
     puts"#{monster.name}の攻撃！"
     damage = (offense - brave.defense) / 2
+    brave.hp = brave.hp - damage
 end
+
+require "./main.rb"
+
+puts "#{monster.name}があらわれた!"
+
+loop do
+  damage = (offense - monster.defense) /2
+  damage = (offense - brave.defense) /2
+  if monster.hp > 0
+    break
+  end
+
+  puts "#{monster.name}をやっつけた！"
+
 
 brave = Brave.new(name: "ゆうしゃ", hp: 238, offense: 203, defense: 129)
 
